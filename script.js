ece70bfa87b32ef14acd7277230433e30b524169
ahadpastpapers <124049277+AR-AbdulAhad@users.noreply.github.com>
2023-02-02 00:02:35 +0500

    agecalculator-web

---
+++ script.js
@@ -0,0 +1,48 @@
+// Age Calculator.....
+function age() {
+    var d1 = document.getElementById("date").value;
+    var m1 = document.getElementById("month").value;
+    var y1 = document.getElementById("year").value;
+
+    var date = new Date();
+    var d2 = date.getDate();
+    console.log(d2)
+    var m2 = 1 + date.getMonth();
+    var y2 = date.getFullYear();
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (d1 > d2) {
+        d2 = d2 + month[m2 - 1];
+        m2 = m2 - 1;
+    }
+
+    if (m1 > m2) {
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+    }
+    var d = d2 - d1;
+    var m = m2 - m1;
+    var y = y2 - y1;
+    document.getElementById("age").innerHTML =
+        "Your Age is: " + y + ":Years, " + m + ":Months, " + d + ":Days";
+}
+
+// Screen Preloader....
+var preloader = document.getElementById("loading");
+
+		function myFunction(){
+			preloader.style.display = 'none';
+		};
+
+// Dark-Mode...
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('icon');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('dark-theme');
+})
+// var change = document.getElementsById("change");
+
+//         change.onclick = function(){
+//             document.body.classList.toggle("dark-theme");
+//         }
